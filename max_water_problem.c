7f3cef19fe41913e1fa49a7453fa43ea4a9444ca
AdityaZDesai <adityadesai753@gmail.com>
2024-06-23 21:43:40 +1000

    Completed in C

--- max_water_problem.c
+++ max_water_problem.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 
-int main(){
-    printf("Hello World \n");
-    returnArray([1, 2, 4]);
-    return 0;
-
-
-}
 
  
-int returnArray(array){
-    for (int i = 0; i < len(array); i++ ){
-        printf(i);
+int returnArray(int arr[], int size){
+    int left = 0;
+    int right = size - 1;
+    int result = 0;
+    while (left != right){
+        if (arr[left] < arr[right]){
+            if (result < arr[left] * (right - left)){
+                result = arr[left] * (right - left);
+                
+            }
+            ++left;
+        }
+        else{
+                if (result < arr[right] * (right - left)){
+                    result = arr[right] * (right - left);
+                    
+                }
+                --right;
+            } 
+            
         
     }
+    return result;
+}
+
+int main(){
+    printf("Hello World \n");
+    int test[100] = {1, 8, 6, 2, 5, 4,8, 3, 7};
+    int len = sizeof(test) / sizeof(test[0]);
+    printf("%d", returnArray(test, len));
     return 0;
+
 
 }
